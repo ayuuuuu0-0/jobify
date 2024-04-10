244378b0575256e302f93f995ddd1da43c84dfba
Ayush Ranjan <ranaayush0725@gmail.com>
2024-04-10 23:43:52 +0530

    new commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,16 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
-import bcrypt from "bcryptjs";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  const user = await User(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 export const login = async (req, res) => {
   //const User = await User.findOne({ email: req
